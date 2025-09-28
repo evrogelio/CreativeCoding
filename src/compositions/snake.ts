cf672cfe59c6e2283201389af3296c21c5b4b99a
Rogelio Echavarria <rogelioev0812@gmail.com>
2025-09-28 11:19:05 -0600

    Snake documentation

--- src/compositions/snake.ts
+++ src/compositions/snake.ts
@@ -3,42 +3,59 @@ import p5 from "p5";
 
 const Y_MARGIN = 100;
 const GRID_SIZE = 20;
-const CELL_SIZE = (window.innerHeight - 2 * Y_MARGIN )/(GRID_SIZE);
-const X_MARGIN = (window.innerWidth - CELL_SIZE * GRID_SIZE)/2;
+const CELL_SIZE = (window.innerHeight - 2 * Y_MARGIN) / (GRID_SIZE);
+const X_MARGIN = (window.innerWidth - CELL_SIZE * GRID_SIZE) / 2;
+
+type Direction = 0 | 90 | 180 | 270; // R U L D
 
 
-type Direction = 0 | 90 | 180 | 270;
-
+// The grid acts as a Game Manager
+// Handles the input events, player direction
+// TODO: 
+//      - Keep score
+//      - Have a special food that just stays there for X amount of time, just gives points
 class Grid {
-  
+
   private pause = false;
   private moveInterval = 200;
   private lastMoveTime = 0;
 
   private p: p5;
+
+  // Array of all the cells flat
+  // [[1,2,3], [4,5,6],[7,8,9]] => [1,2,3,4,5,6,7,8,9]
   private cells: Cell[] = [];
+
+  // Only keeps track of the head, anthing else does not matter
   private snakeHead: SnakeBlock;
 
   private playerDirection: Direction = 0;
 
+  // Fixes bug in movement
   hasAdvanceInThisCurrentDir: boolean = false;
 
-  constructor(p: p5){
+  constructor(p: p5) {
     this.p = p;
-    for(let i = 0; i < GRID_SIZE; i++){
-      for(let j = 0; j < GRID_SIZE; j++) {
+    for (let i = 0; i < GRID_SIZE; i++) {
+      for (let j = 0; j < GRID_SIZE; j++) {
         this.cells.push(new Cell(GRID_SIZE * i + j, p))
       }
     }
-    this.snakeHead = new SnakeBlock(this.cells[(this.cells.length - GRID_SIZE -2)/2], this.p);
-    this.snakeHead.attachChild(new SnakeBlock(this.cells[this.snakeHead.position.index + 2], p))
-    // this.snakeHead.attachChild(new SnakeBlock(this.cells[this.snakeHead.position.index + 3], p))
+
+    // Spawns head in the "middle"
+    this.snakeHead = new SnakeBlock(this.cells[(this.cells.length - GRID_SIZE - 2) / 2], this.p);
+    // Spawns first tail block
+    this.snakeHead.attachChild(new SnakeBlock(this.cells[this.snakeHead.position.index + 1], p))
 
     this.generateFood()
   }
 
-  move(){
-    if(this.pause) return;
+  move() {
+    if (this.pause) return;
+
+    // Stores how to get to the next cell in the array 
+    // L and R movements are +- 1
+    // U and D movements are +- GRID_SIZE, or skip a whole row
     let modifier = 0;
     switch (this.playerDirection) {
       case 0:
@@ -54,39 +71,51 @@ class Grid {
         modifier = GRID_SIZE;
         break;
     }
+
+    // Validates Collision with borders and snake blocks
+    // Returns the cell to move the head or undefined if collided
     const nextCell = this.validateCollisions(this.snakeHead.position.index, modifier);
 
-    if(nextCell == undefined) return this.endGame();
+    // Ends the game
+    if (nextCell == undefined) return this.endGame();
 
+    // Moves the head, and in that the connected blocks
+    // Returns the cell where the last block WAS
+    // If the next block is food, spawns a new block there
     const lastCell = this.snakeHead.move(nextCell);
-    if(nextCell.isFood) {
+    if (nextCell.isFood) {
 
       this.snakeHead.attachChild(new SnakeBlock(lastCell, this.p));
       nextCell.isFood = false;
       this.generateFood()
     }
-    this.hasAdvanceInThisCurrentDir = true; 
+
+    // Part of the movement bugfix
+    this.hasAdvanceInThisCurrentDir = true;
   }
-  
+
   changeDirection(direction: Direction) {
-    if(Math.abs(this.playerDirection - direction) == 180 || !this.hasAdvanceInThisCurrentDir) return
+    // Can only change direction after you have advanced 1 block
+    if (Math.abs(this.playerDirection - direction) == 180 || !this.hasAdvanceInThisCurrentDir) return
     this.playerDirection = direction;
     this.hasAdvanceInThisCurrentDir = false;
   }
 
-  generateFood(){
+  // Randomly selects a cell to make it food 
+  // If the cell has a snake block, checks the one to the right
+  generateFood() {
     let randCellIndex = Math.floor(Math.random() * GRID_SIZE * GRID_SIZE);
     let cell = this.cells[randCellIndex];
-    while(cell.snakeBlock){
+    while (cell.snakeBlock) {
       randCellIndex++;
-      if(randCellIndex == GRID_SIZE * GRID_SIZE) randCellIndex = 0;
+      if (randCellIndex == GRID_SIZE * GRID_SIZE) randCellIndex = 0;
       cell = this.cells[randCellIndex];
     }
 
     cell.isFood = true;
 
   }
-  
+
 
   // Checks collisions with border of grid and with snake
   // Returns the cell where the head should be next, or undefined if collided
@@ -99,95 +128,129 @@ class Grid {
 
     // Get next cell
     const nextCell = this.cells[currentIndex + modifier];
-    
+
     // Check if collides with snake
     if (nextCell.snakeBlock) return undefined;
-    
+
+    // Returns the valid block
     return nextCell;
   }
 
-  endGame(){
+  endGame() {
+    // Just pauses the game
     console.log('Game Over');
     this.pause = true;
   }
 
-  draw(){
+  draw() {
 
-    if(!(this.p.millis() - this.lastMoveTime > this.moveInterval)) return;
-    
+    // Only applies movement every moveInterval milliseconds
+    // TODO: reduce the moveInterval parameter proportional to the score to make it harder
+    if (!(this.p.millis() - this.lastMoveTime > this.moveInterval)) return;
+
     this.move();
     this.lastMoveTime = this.p.millis();
-    this.cells.forEach(cell=>cell.draw()); 
+    this.cells.forEach(cell => cell.draw());
     this.snakeHead.draw();
-    
+
   }
 }
 
-
+// The building block of a snake
+// Resembles a linked list:
+// Each block only knows of the next block
 class SnakeBlock {
 
+  // Stores a reference to the next block, can be empty
   private child: SnakeBlock | undefined;
 
+
+  // Keeps a reference of its position
   position: Cell;
 
   private p: p5;
 
-  constructor(position: Cell, p: p5){
+  constructor(position: Cell, p: p5) {
     this.position = position;
     this.p = p;
   }
-  
 
-  draw(){
+
+  draw() {
     this.p.fill(20);
     this.p.rect(...this.position.getCoordinates(), CELL_SIZE);
-    if(this.child) this.child.draw();
+    if (this.child) this.child.draw();
   }
 
-  move(newPos: Cell): Cell{
+
+  move(newPos: Cell): Cell {
+
+    // Makes its cell an empty cell
     this.position.snakeBlock = false;
 
+    // Asumes this is the last block and makes its position the last position 
+    // This is used as the place where new blocks are spawned
     let lastCell: Cell = this.position;
-    if(this.child) lastCell = this.child.move(this.position);
-    
-    this.position = newPos;
-    newPos.snakeBlock = true;
-    return lastCell;
-    
-  }
 
-  attachChild(child: SnakeBlock): void{
-    if(this.child) return this.child.attachChild(child)
+    // Moves its child if exists, its child returns its last position
+    // Overwrites the last position with the child last position
+    //
+    // This happens sort of recursively, the last cell gets replaced with the child lastCell until theres no child
+    // That gets returned up the chain until it gets to the head snakeBlock, that returns it to the Grid
+    if (this.child) lastCell = this.child.move(this.position);
+
+
+    // Updates its own new position
+    this.position = newPos;
+    // Tells the cell now has a snakeblock
+    newPos.snakeBlock = true;
+
+    // Returns the last cell
+    return lastCell;
+
+  }
+  // Another "recursive" method
+  attachChild(child: SnakeBlock): void {
+    // If theres already a child in this snakeBlock, this method get executed with the child
+    // So, if the child has a child, it gets executed with that blocks
+    // Eventually a child wont have a child of its own
+    if (this.child) return this.child.attachChild(child)
+
+    // Then it gets stored
     this.child = child;
   }
 }
 
-
+// Main block of the game area
 class Cell {
 
   private px: number;
   private py: number;
   private p: p5;
   index: number;
-  
+
   isFood: boolean = false;
   snakeBlock: boolean = false;
 
-  constructor(index:number, p: p5) {
+  constructor(index: number, p: p5) {
+
+    // Calculates the pixel postion based on the index
     this.index = index;
     this.px = (index % GRID_SIZE) * CELL_SIZE + X_MARGIN;
-    this.py = Math.floor(index/GRID_SIZE) * CELL_SIZE + Y_MARGIN;
+    this.py = Math.floor(index / GRID_SIZE) * CELL_SIZE + Y_MARGIN;
     this.p = p;
   }
 
   draw() {
+    // Draws a box, gray if normal, white if food
     this.p.fill(128);
-    if(this.isFood) this.p.fill(255);
+    if (this.isFood) this.p.fill(255);
     this.p.rect(this.px, this.py, CELL_SIZE);
 
   }
 
   getCoordinates(): [number, number] {
+    // Returns an array of the x, y coordinates
     return [this.px, this.py]
   }
 }
@@ -210,6 +273,7 @@ function resize(p: p5) {
   p.background(0);
 }
 
+// WASD Controller
 function inputManager(key: string, grid: Grid) {
   switch (key) {
     case 'w':
